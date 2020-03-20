09b2b01ca1dbb168778f29eb2dfbe1674961e551
Cedric Verstraeten <cedric@verstraeten.io>
2020-03-20 13:37:17 +0100

    change div + styling

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -8,20 +8,22 @@ const useStyles = makeStyles(theme => ({
       height: '100vh',
     },
     image: {
-      backgroundImage: 'url("/streetlights.png/")',
+      backgroundImage: 'url("./streetlights.png")',
       backgroundRepeat: 'no-repeat',
       backgroundColor:
         theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
       backgroundSize: 'cover',
       backgroundPosition: 'center',
+      height: "100vh",
+      width: "100vw",
+      display: "table"
     },
-}));   
+}));
 export default function SignInSide() {
     const classes = useStyles();
-      
+
     return (
-      <item xs={false} sm={4} md={7} className={classes.image} />
-       
+      <div xs={false} sm={4} md={7} className={classes.image}>hello</div>
+
     );
 }
-
