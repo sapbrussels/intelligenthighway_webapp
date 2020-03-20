1dc6df148f63f6043b2ac6cc1bbb588462ff5ae2
NIco T-S <ntatesmith@hotmail.com>
2020-03-20 12:30:29 +0100

    trying to upload image

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+import { makeStyles } from '@material-ui/core/styles';
 
-function App() {
-  return (
-  <img src="/streetlights.png"/>
-  );
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      height: '100vh',
+    },
+    image: {
+      backgroundImage: 'url("/streetlights.png/")',
+      backgroundRepeat: 'no-repeat',
+      backgroundColor:
+        theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+}));   
+export default function SignInSide() {
+    const classes = useStyles();
+      
+    return (
+      <item xs={false} sm={4} md={7} className={classes.image} />
+       
+    );
 }
 
-export default App;
